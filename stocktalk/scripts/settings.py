fa298f4bc9d91b7460f5d0dfc673d5afb52ec174
crappybones <lawrence@blockcelerate.com>
2018-05-09 00:46:26 +0800

    Update settings.py

--- stocktalk/scripts/settings.py
+++ stocktalk/scripts/settings.py
@@ -1,13 +1,13 @@
 # Mongo
-mongo_server = 'ds254236.mlab.com' # Find this in your deployment settings
-mongo_id     =  54236              # Find this in your deployment settings
+mongo_server = 'ds117540.mlab.com' # Find this in your deployment settings
+mongo_id     =  17540              # Find this in your deployment settings
 mongo_client = 'stocktalk'         # The name of your deployment
-mongo_user   = 'username'          # The username you added as a user to your deployment
-mongo_pass   = 'password'          # The password you made for that user
+mongo_user   = 'Crappybones'          # The username you added as a user to your deployment
+mongo_pass   = 'durexcondom123'          # The password you made for that user
 
 # Twitter
-api_key             = ''
-api_secret          = ''
-access_token        = ''
-access_token_secret = ''
+api_key             = 'f4z29gyFZYFBKFiwxzGLS8bU4'
+api_secret          = '556dwIEfYntZXp1rtHy5eZ704iv3g5OBYEBJOecxudJ15fEVNn'
+access_token        = '871985854104952832-gDa04PrfOfW6fSn7jl1QMnnMCDJGIhL'
+access_token_secret = 'c59x3SrENnG6hAmnlV9aQVqwSCBhP6v14XPJHUcBPxuWy'
 credentials = [api_key, api_secret, access_token, access_token_secret]
